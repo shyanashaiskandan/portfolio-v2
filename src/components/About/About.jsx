f47a34f279c310f415f297f48b774c730eb34e10
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2025-01-03 14:48:40 -0500

    wip about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import styles from "./About.module.css"
 
 export default function About() {
   return (
-    <div>
-      Hello There
-      <p>isjfosijfodsifj</p>
-      <h1>isfjosifjofsi</h1>
-    </div>
+    <section id="about" className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I'm Shyana!</h1>
+            <p className={styles.description}>I'm a fourth year Computer Engineering Student at the University of Waterloo.
+
+I'm passionate about technology and constantly curious about the world of software.
+
+I'm currently exploring full-stack development , learning how to connect frontend and backend systems to build more complete solutions.
+
+Let's build something amazing together!</p>
+            <a href="#projects" className={styles.btn}>Check out my work!</a>
+        </div>
+        <img src="/assets/about/heroImage.png" alt="image of me" className={styles.profileImage}/>
+    <div className={styles.topBlur}></div>
+    <div className={styles.bottomBlur}></div>
+    </section>
   )
 }
