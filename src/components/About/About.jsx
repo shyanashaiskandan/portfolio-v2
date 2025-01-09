b812fabe2cae0daeed6340a74909524d3ab269d5
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2025-01-08 20:54:57 -0500

    about page color scheme

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,20 +2,31 @@ import React from 'react'
 import styles from "./About.module.css"
 
 export default function About() {
-  return (
-    <section id="about" className={styles.container}>
+    return (
+      <section id="about" className={styles.container}>
         <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I'm Shyana!</h1>
-            <p className={styles.description}>I'm a fourth year Computer Engineering Student at the University of Waterloo. </p>
-            <p className={styles.description}>I'm passionate about technology and constantly curious about the world of software.</p>
-            <p className={styles.description}>I'm currently exploring full-stack development , learning how to connect frontend and backend systems to build more complete solutions.</p>
-            <p className={styles.description}>Let's build something amazing together!</p>
-
-            <a href="#projects" className={styles.btn}>Check out my work!</a>
+          <h1 className={styles.title}>Hi, I'm Shyana!</h1>
+          <p className={styles.description}>
+            I'm a fourth-year Computer Engineering Student at the 
+            <span className={styles.highlightBlue}> University of Waterloo</span>.
+          </p>
+          <p className={styles.description}>
+            I'm passionate about technology and constantly curious about the world of software.
+          </p>
+          <p className={styles.description}>
+            I'm currently exploring 
+            <span className={styles.highlightBlue}> full-stack development</span>, learning how to connect frontend and backend systems to build more complete solutions.
+          </p>
+          <p className={`${styles.description} ${styles.highlightGreen}`}>
+          <span className={styles.highlightGreen}> Let's build something amazing together! </span>
+          </p>
+  
+          <a href="#projects" className={`${styles.btn} ${styles.btnYellow}`}>Check out my work!</a>
         </div>
-        <img src="/assets/about/heroImage.png" alt="image of me" className={styles.profileImage}/>
-    <div className={styles.topBlur}></div>
-    <div className={styles.bottomBlur}></div>
-    </section>
-  )
-}
+        <img src="/assets/about/heroImage.png" alt="image of me" className={styles.profileImage} />
+        <div className={styles.topBlur}></div>
+        <div className={styles.bottomBlur}></div>
+      </section>
+    );
+  }
+  
