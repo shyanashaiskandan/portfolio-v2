7c70b461ef89bb749af19184e3445b76c8d5cac1
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2025-01-09 18:36:07 -0500

    experience css

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,70 +1,125 @@
 import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import bmo from "./bmologo.png"
-import manulife from "./manulife logo.png"
-import gov from "./governmentlogo.png"
-import styles from "./Experience.module.css"
-
+import bmo from "./bmologo.png";
+import manulife from "./manulife logo.png";
+import gov from "./governmentlogo.png";
+import styles from "./Experience.module.css";
+import empireLife from "/assets/experience/empirelife.png"
 
 const Experience = () => {
   return (
-    <section id="experience" className = {styles.container}>
-      
-      <h1 className = {styles.title}><center>Experience</center></h1>
-      <VerticalTimeline>
-    
-      <VerticalTimelineElement visible={true}
-            className="vertical-timeline-element"
-            contentStyle={{background: 'rgb(245, 143, 124)', color: 'white' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(245, 143, 124)' }}
-            date="January 2024 - April 2024"
-            iconStyle={{ background: 'rgb(245, 143, 124)', color: '#4f4f51' }}  
-          >
-          <center><img src= {gov} alt="gov logo" className={styles.govlogo}/></center>
-          <center><h3>Junior Programmer</h3></center>
-          <center><h4>Ministry of Finance</h4></center>
+    <section id="experience" className={styles.container}>
+      <h1 className={styles.title}><center>Experience</center></h1>
+      <VerticalTimeline lineColor='black'>
+        
+        {/* Empire Life */}
+        <VerticalTimelineElement
+          visible={true}
+          className="vertical-timeline-element"
+          contentStyle={{
+            background: '#d97b9f',
+            color: 'black',
+            border: '2px solid black',
+          }}
+          contentArrowStyle={{
+            borderRight: '7px solid black', // Arrow with black border
+          }}
+          date={<span style={{ color: '#9e849e' }}>January 2025 - April 2025</span>}
+          iconStyle={{ background: '#b16687', color: 'white' }}
+        >
+          <center><img src={empireLife} alt="gov logo" className={styles.empireLife} /></center>
+          <center><h3 style={{ color: '#ffffff' }}>Software Developer</h3></center>
+          <center><h4 style={{ color: '#f9f1f1' }}>Empire Life</h4></center>
+        </VerticalTimelineElement>
+        
+        
+        
+        
+        
+        
+        {/* Ministry of Finance */}
+        <VerticalTimelineElement
+          visible={true}
+          className="vertical-timeline-element"
+          contentStyle={{
+            background: '#d97b9f',
+            color: 'black',
+            border: '2px solid black',
+          }}
+          contentArrowStyle={{
+            borderRight: '7px solid black', // Arrow with black border
+          }}
+          date={<span style={{ color: '#9e849e' }}>January 2024 - April 2024</span>}
+          iconStyle={{ background: '#b16687', color: 'white' }}
+        >
+          <center><img src={gov} alt="gov logo" className={styles.govlogo} /></center>
+          <center><h3 style={{ color: '#ffffff' }}>Junior Programmer</h3></center>
+          <center><h4 style={{ color: '#f9f1f1' }}>Ministry of Finance</h4></center>
         </VerticalTimelineElement>
 
-        <VerticalTimelineElement visible={true}
-            className="vertical-timeline-element"
-            contentStyle={{background: 'rgb(245, 143, 124)', color: 'white' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(245, 143, 124)' }}
-            date="May 2023 - August 2023"
-            iconStyle={{ background: 'rgb(245, 143, 124)', color: '#4f4f51' }}  
-          >
-          <center><img src= {manulife} alt="manulife logo" className={styles.manulife}/> </center>
-          <center><h3>Salesforce Engineer</h3></center>
-          <center><h4>Manulife</h4></center>
+        {/* Manulife */}
+        <VerticalTimelineElement
+          visible={true}
+          className="vertical-timeline-element"
+          contentStyle={{
+            background: '#d97b9f',
+            color: 'white',
+            border: '2px solid black',
+          }}
+          contentArrowStyle={{
+            borderRight: '7px solid black', // Arrow with black border
+          }}
+          date={<span style={{ color: '#9e849e' }}>May 2023 - August 2023</span>}
+          iconStyle={{ background: '#b16687', color: 'white' }}
+        >
+          <center><img src={manulife} alt="manulife logo" className={styles.manulife} /></center>
+          <center><h3 style={{ color: '#ffffff' }}>Salesforce Engineer</h3></center>
+          <center><h4 style={{ color: '#f9f1f1' }}>Manulife</h4></center>
         </VerticalTimelineElement>
 
-        <VerticalTimelineElement visible={true}
-            className="vertical-timeline-element"
-            contentStyle={{background: 'rgb(245, 143, 124)', color: 'white' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(245, 143, 124)' }}
-            date="September 2022 - December 2022"
-            iconStyle={{ background: 'rgb(245, 143, 124)', color: '#4f4f51' }}  
-          >
-          <center><img src= {bmo} alt="BMO logo" className={styles.bmo}/> </center>
-          <center><h3>Systems and Applications Analyst</h3></center>
-          <center><h4>Bank of Montreal</h4></center>
+        {/* Bank of Montreal - Systems Analyst */}
+        <VerticalTimelineElement
+          visible={true}
+          className="vertical-timeline-element"
+          contentStyle={{
+            background: '#d97b9f',
+            color: 'white',
+            border: '2px solid black',
+          }}
+          contentArrowStyle={{
+            borderRight: '7px solid black', // Arrow with black border
+          }}
+          date={<span style={{ color: '#9e849e' }}>September 2022 - December 2022</span>}
+          iconStyle={{ background: '#b16687', color: 'white' }}
+        >
+          <center><img src={bmo} alt="BMO logo" className={styles.bmo} /></center>
+          <center><h3 style={{ color: '#ffffff' }}>Systems and Applications Analyst</h3></center>
+          <center><h4 style={{ color: '#f9f1f1' }}>Bank of Montreal</h4></center>
         </VerticalTimelineElement>
 
-        <VerticalTimelineElement visible={true}
-            className="vertical-timeline-element"
-            contentStyle={{background: 'rgb(245, 143, 124)', color: 'white' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(245, 143, 124)' }}
-            date="January 2022 - April 2022"
-            iconStyle={{ background: 'rgb(245, 143, 124)', color: '#4f4f51' }}  
-          >
-          <center><img src= {bmo} alt="BMO logo" className={styles.bmo}/> </center>
-          <center><h3 >QA Analyst</h3></center>
-          <center><h4>Bank of Montreal</h4></center>
-        </VerticalTimelineElement> 
-
-    </VerticalTimeline> 
+        {/* Bank of Montreal - QA Analyst */}
+        <VerticalTimelineElement
+          visible={true}
+          className="vertical-timeline-element"
+          contentStyle={{
+            background: '#d97b9f',
+            color: 'white',
+            border: '2px solid black',
+          }}
+          contentArrowStyle={{
+            borderRight: '7px solid black', // Arrow with black border
+          }}
+          date={<span style={{ color: '#9e849e' }}>January 2022 - April 2022</span>}
+          iconStyle={{ background: '#b16687', color: 'white' }}
+        >
+          <center><img src={bmo} alt="BMO logo" className={styles.bmo} /></center>
+          <center><h3 style={{ color: '#ffffff' }}>QA Analyst</h3></center>
+          <center><h4 style={{ color: '#f9f1f1' }}>Bank of Montreal</h4></center>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
