3538e8c9a45fb2fbe4816c2f6447f5bdd700ca43
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2025-04-21 13:20:11 -0400

    fix build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar';
-import Title from './components/Title/title';
+import Title from './components/Title/Title';
 import About from './components/About/About';
 import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
