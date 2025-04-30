f731a058c3c0cd3ccfb224003600a87552a7507d
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2025-04-30 11:43:00 -0400

    heart png

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,22 +3,23 @@ import styles from './Contact.module.css';
 import github from "/assets/Contact/github.png";
 import linkedin from "/assets/Contact/linkedin.png";
 import email from "/assets/Contact/email.png";
+import test from "/assets/Contact/test.png";
 
 const Contact = () => {
   return (
     <div id="contact" className={styles.contactCard}>
       <div className={styles.imageContainer}>
         <img
-          src="/assets/Contact/test.png"
-          alt="Profile"
+          src={test}
+          alt="Heart"
           className={styles.profileImage}
         />
       </div>
       <div className={styles.infoContainer}>
         <h2 className={styles.name}>Reach Out to Me!</h2>
         <p className={styles.title}>
-          I’m constantly learning and love connecting with others who are just as passionate about technology. 
-          Let’s chat :)
+          I'm constantly learning and love connecting with others who are just as passionate about technology. 
+          Let's chat :)
         </p>
         {/* <div className={styles.contactInfo}>
           <p>
