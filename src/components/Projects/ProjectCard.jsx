6a91e4c36289f8b47eb08490fff867e8b3319e42
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2025-05-02 14:28:12 -0400

    update vercel pngs

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import styles from "./ProjectCard.module.css";
 import githubLogo from "/assets/projects/githublogo.png";
+import photoboothImage from "/assets/projects/photobooth.png";
+import geographyQuizImage from "/assets/projects/geographyquiz.png";
+import projectBImage from "/assets/projects/project.png";
 
 const ProjectCard = ({ project }) => {
   const { title, imageSrc, description, skills, demo, source } = project;
 
   // Determine if this is the Geography Quiz project
   const isGeographyQuiz = imageSrc.includes('geographyquiz');
+  
+  // Map image paths to imported images
+  const imageMap = {
+    'assets/projects/photobooth.png': photoboothImage,
+    'assets/projects/geographyquiz.png': geographyQuizImage,
+    'assets/projects/project.png': projectBImage
+  };
 
   return (
     <div className={styles.container}>
       <img 
-        src={imageSrc} 
+        src={imageMap[imageSrc] || imageSrc} 
         alt={title} 
         className={`${styles.image} ${isGeographyQuiz ? styles.quizImage : ''}`}
       />
