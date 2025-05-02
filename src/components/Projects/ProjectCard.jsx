f8f3a75150a7344493d58df93e39c50df21bb02d
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2025-05-02 14:14:12 -0400

    updated photobooth project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import styles from "./ProjectCard.module.css";
 import githubLogo from "/assets/projects/githublogo.png";
-import projectImage from "/assets/projects/project.png";
-import geographyQuizImage from "/assets/projects/geographyquiz.png";
 
 const ProjectCard = ({ project }) => {
   const { title, imageSrc, description, skills, demo, source } = project;
-
-  // Select the appropriate image based on the imageSrc
-  const getImage = () => {
-    if (imageSrc.includes('geographyquiz')) return geographyQuizImage;
-    return projectImage;
-  };
 
   // Determine if this is the Geography Quiz project
   const isGeographyQuiz = imageSrc.includes('geographyquiz');
@@ -19,7 +11,7 @@ const ProjectCard = ({ project }) => {
   return (
     <div className={styles.container}>
       <img 
-        src={getImage()} 
+        src={imageSrc} 
         alt={title} 
         className={`${styles.image} ${isGeographyQuiz ? styles.quizImage : ''}`}
       />
