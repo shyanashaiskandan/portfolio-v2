c0273153637feff35f093a781c07a29bd822f078
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2025-05-05 16:58:15 -0400

    added movie recommendations project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,19 +3,17 @@ import styles from "./ProjectCard.module.css";
 import githubLogo from "/assets/projects/githublogo.png";
 import photoboothImage from "/assets/projects/photobooth.png";
 import geographyQuizImage from "/assets/projects/geographyquiz.png";
-import projectBImage from "/assets/projects/project.png";
+import movieRecommendationsImage from "/assets/projects/movie-recommendations.png";
 
 const ProjectCard = ({ project }) => {
   const { title, imageSrc, description, skills, demo, source } = project;
 
-  // Determine if this is the Geography Quiz project
   const isGeographyQuiz = imageSrc.includes('geographyquiz');
   
-  // Map image paths to imported images
   const imageMap = {
     'assets/projects/photobooth.png': photoboothImage,
     'assets/projects/geographyquiz.png': geographyQuizImage,
-    'assets/projects/project.png': projectBImage
+    'assets/projects/movie-recommendations.png': movieRecommendationsImage
   };
 
   return (
